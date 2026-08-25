d55f3bec7848a1984fa4acdbdc305f89b15d64a7
Santosh PurpleLane <161108010+santoshpl@users.noreply.github.com>
2026-08-25 13:36:36 +0530

    Restore React shop context exactly

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -28,25 +28,48 @@ export const ShopProvider = ({ children }) => {
         setCart((prevCart) => {
             const existingItem = prevCart.find((item) => item._id === product._id);
             if (existingItem) {
-                return prevCart.map((item) => item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item);
+                return prevCart.map((item) =>
+                    item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
+                );
             }
             return [...prevCart, { ...product, quantity: 1 }];
         });
     };
 
-    const removeFromCart = (productId) => setCart((prevCart) => prevCart.filter((item) => item._id !== productId));
-
-    const updateQuantity = (productId, amount) => {
-        setCart((prevCart) => prevCart.map((item) => {
-            if (item._id === productId) {
-                const newQuantity = item.quantity + amount;
-                return newQuantity > 0 ? { ...item, quantity: newQuantity } : item;
-            }
-            return item;
-        }));
+    const removeFromCart = (productId) => {
+        setCart((prevCart) => prevCart.filter((item) => item._id !== productId));
     };
 
-    const clearCart = () => setCart([]);
+    const updateQuantity = (productId, amount) => {
+        setCart((prevCart) =>
+            prevCart.map((item) => {
+                if (item._id === productId) {
+                    const newQuantity = item.quantity + amount;
+                    return newQuantity > 0 ? { ...item, quantity: newQuantity } : item;
+                }
+                return item;
+            })
+        );
+    };
 
-    return <ShopContext.Provider value={{ products, cart, loading, fetchProducts, addToCart, removeFromCart, updateQuantity, clearCart }}>{children}</ShopContext.Provider>;
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    return (
+        <ShopContext.Provider
+            value={{
+                products,
+                cart,
+                loading,
+                fetchProducts,
+                addToCart,
+                removeFromCart,
+                updateQuantity,
+                clearCart,
+            }}
+        >
+            {children}
+        </ShopContext.Provider>
+    );
 };
