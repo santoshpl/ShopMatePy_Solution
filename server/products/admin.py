456eae93e25b14b73b82b0be9ada41f25fef5bb2
Santosh PurpleLane <161108010+santoshpl@users.noreply.github.com>
2026-08-25 13:34:38 +0530

    Register Product in Django admin

---
+++ server/products/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "stock", "created_at")
+    search_fields = ("name", "category")
+    list_filter = ("category",)
