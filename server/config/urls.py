277b60c3a0743362a2b5c30ec000c000c183f78b
Santosh PurpleLane <161108010+santoshpl@users.noreply.github.com>
2026-08-25 13:33:59 +0530

    Add API URL configuration

---
+++ server/config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/products/", include("products.urls")),
+    path("api/ai/", include("ai.urls")),
+]
